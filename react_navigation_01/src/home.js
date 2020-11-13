a8af97aec4a57a2a5a693a94cbd831843f0cc20f
heyoni <hhheyoni@gmail.com>
2020-11-14 01:27:13 +0900

    (1113) Navigation Params+TextInput
    TextInput에 입력받은 값을 다음 페이지로 넘겨줌

--- react_navigation_01/src/home.js
+++ react_navigation_01/src/home.js
@@ -8,18 +8,59 @@
 import 'react-native-gesture-handler';
 import React, { Component } from 'react';
 import { View, Text, Button } from 'react-native';
+import { TextInput } from 'react-native';
+import { event } from 'react-native-reanimated';
+import { TouchableOpacity } from 'react-native';
 
 //리엑트 네비게이션에서는 데이터를 보낸다 : 파라미터를 루트로 passing한다 라는 뜻
 //home -> user로 데이터를 보낼것
 class HomeScreen extends Component {
+  state = {
+      myIdx : '',
+      myName : '',
+      myLastName : ''
+  }
+  onChagneInput_idx = (event) =>{
+      this.setState({
+          myIdx:event
+      })
+  }
+  onChagneInput_name = (event) =>{
+    this.setState({
+          myName:event
+      })
+  }
+  onChagneInput_lstName = (event) =>{
+    this.setState({
+        myLastName:event
+      })
+  }
   render() {
     return(
         <View style={{
             flex:1,
-             alignItems:'center',
+            alignItems:'center',
             justifyContent:'center'
         }}>
           <Text>home screen</Text>
+          <TextInput
+            value={this.state.myIdx}
+            style={{ width:100, height: 40, borderColor: 'gray', borderWidth: 1}}
+            onChangeText={this.onChagneInput_idx}
+            autoCapitalize={'none'}
+          />
+          <TextInput
+            value={this.state.myName}
+            style={{ width:100, height: 40, borderColor: 'gray', borderWidth: 1}}
+            onChangeText={this.onChagneInput_name}
+            autoCapitalize={'none'}
+          />
+          <TextInput
+            value={this.state.myLastName}
+            style={{ width:100, height: 40, borderColor: 'gray', borderWidth: 1}}
+            onChangeText={this.onChagneInput_lstName}
+            autoCapitalize={'none'}
+          />
           <Button 
             title="user screen으로 이동하기"
             //터치이벤트
@@ -36,13 +77,14 @@ class HomeScreen extends Component {
                     //객체를 넣어줌
                     //이 값들을 userScreen으로 넘겨줄거고 파라미터라고 부름 
                     //이 값들이 유저스크린의 루트가 됨
-                    userIdx: 100,
-                    userName: 'hyewon',
-                    userLastName:'Kim'
+                    userIdx: this.state.myIdx,
+                    userName: this.state.myName,
+                    userLastName: this.state.myLastName
                 })//App.js에서 정의한 name값을 인자로 넣어줌
 
             }}
           />
+              
         </View>
     )
   }
