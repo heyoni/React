15699ca0b332f9befb1409054110ed3cce8c89be
heyoni <hhheyoni@gmail.com>
2020-11-19 00:19:15 +0900

    (1118) Drawer Style1
    drawer style 설정하기

--- react_navigation_01/App.js
+++ react_navigation_01/App.js
@@ -9,7 +9,12 @@ import 'react-native-gesture-handler';
 import React, { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from "@react-navigation/stack";
-import { createDrawerNavigator } from "@react-navigation/drawer"
+import { 
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem
+ } from "@react-navigation/drawer"
 import { View, Text, StyleSheet, Image, Button } from 'react-native';
 // 리엑트 네비게이션을 사용하기 위해서는 네비게이션 컨테이너라는 태그에 감싸줘야함
 // 그전에 임포터해줘야 하는 요소들임 
@@ -19,8 +24,24 @@ import UserScreen from './src/user';
 import LogoTitle from './src/logo'
 import DrawerHomeScreen from './src/home_drawer'
 import DrawerUserScreen from './src/user_drawer'
+import { Linking } from 'react-native';
 
-
+CustomDrawerContent = (props) =>{
+  return (
+    //스크롤 뷰, 아이템 리스트를 반환해 줄 예정
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props}/>
+      <DrawerItem 
+        label='help'
+        onPress={()=>Linking.openURL('http://www.google.com')}
+      />
+      <DrawerItem 
+        label='info'
+        onPress={()=>alert('info Window')}
+      />
+    </DrawerContentScrollView>
+  )
+}
 
 // 스크린, 네비게이터를 리턴하는 프로퍼티
 // 랜딩하는 부분에서 stack.screen으로 사용함
@@ -31,15 +52,46 @@ const Drawer = createDrawerNavigator();
 //현재는 home이 먼저 작성되어 있기 때문에 홈이 제일 먼저 띄워지나,
 //userScreen을 먼저 띄우고 싶다면 Navigator 옆에 initialRouteName ="user"로 주면 됨
 class App extends Component {
+
+
+  //스타일 한가지만 적용 : stack.screen options
+  //공통 : stack.navigator screenOptions
+  //drawNavgator는 모든 스크린에서 열리기 때문에 스타일이 공통으로 적용되기 때문에 draw.navigator 태그 안에서 스타일 적용함
   render() {
     return (
       <NavigationContainer>
-        <Drawer.Navigator>
+        <Drawer.Navigator
+          initialRouteName="Home"
+          drawerType="front"
+          // drawerType='slide' //화면 같이 밀림
+          // drawerType='permanent' //영원히 열림
+          drawerPosition='right' //오른쪽에서 열림
+          drawerStyle={{
+            backgroundColor:'#c6cbef',
+            width:200
+          }}
+          // 다른 페이지로 갈 수 있는 버튼 스타일주기
+          drawerContentOptions={{
+            activeTintColor:'red',
+            activeBackgroundColor:'skyblue'
+          }}
+          //drawerContetn를 커스터마이징함
+          //렌더링하기 위한 리엑트 요소를 반환함
+          //4개의 props를 받음
+          // 1. drawe navigator에 어떤 요소들이 있는지 알려주는 state
+          // 2. 화면 이동을 위한 navigation
+          // 3. drawer screen에 옵션을 담기위한 디스크립터스
+          // 4. drew가 열렸는지 판단하는 progress
+          //대충 이해하되, 4개의 props를 전달해줘야하구나 정도로만 알면 됨
+          drawerContent={props => <CustomDrawerContent {...props} />}
+        >
           <Drawer.Screen name="Home" component={DrawerHomeScreen}/>
           <Drawer.Screen name="User" component={DrawerUserScreen}/>
         </Drawer.Navigator>
       </NavigationContainer>
+
   // logoTitle = () =>{
+  //   return(
   //     <Image 
   //       style={{width: 40, height: 40}}
   //       source={require('./src/assets/pics/home_icon.png')}
