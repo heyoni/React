aeabec0f0ce94856ba55e5772b6c59d2c4d47545
heyoni <hhheyoni@gmail.com>
2020-11-19 23:55:46 +0900

    (1119) Tab
    Tab 추가

--- react_navigation_01/App.js
+++ react_navigation_01/App.js
@@ -9,6 +9,8 @@ import 'react-native-gesture-handler';
 import React, { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
 import { 
   createDrawerNavigator,
   DrawerContentScrollView,
@@ -27,6 +29,8 @@ import DrawerUserScreen from './src/user_drawer'
 import { Linking } from 'react-native';
 import PictogramHome from './src/assets/pics/home_icon.png'
 import SideDrawer from './src/my_drawer'
+import TabHomeScreen from './src/home_tab'
+import TabUserScreen from './src/user_tab'
 
 
 
@@ -53,6 +57,7 @@ import SideDrawer from './src/my_drawer'
 // 랜딩하는 부분에서 stack.screen으로 사용함
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
 
 //route : home(홈스크린을 컴포넌트로 갖는 홈루트), user(유저스크린을 컴포넌트로 갖는 유저루트)
 //현재는 home이 먼저 작성되어 있기 때문에 홈이 제일 먼저 띄워지나,
@@ -66,48 +71,54 @@ class App extends Component {
   render() {
     return (
       <NavigationContainer>
-        <Drawer.Navigator
-          initialRouteName="Home"
-          drawerType="front"
-          // drawerType='slide' //화면 같이 밀림
-          // drawerType='permanent' //영원히 열림
-          drawerPosition='right' //오른쪽에서 열림
-          drawerStyle={{
-            backgroundColor:'#c6cbef',
-            width:200
-          }}
-          // 다른 페이지로 갈 수 있는 버튼 스타일주기
-          drawerContentOptions={{
-            activeTintColor:'red',
-            activeBackgroundColor:'skyblue'
-          }}
-          //drawerContetn를 커스터마이징함
-          //렌더링하기 위한 리엑트 요소를 반환함
-          //4개의 props를 받음
-          // 1. drawe navigator에 어떤 요소들이 있는지 알려주는 state
-          // 2. 화면 이동을 위한 navigation
-          // 3. drawer screen에 옵션을 담기위한 디스크립터스
-          // 4. drew가 열렸는지 판단하는 progress
-          //대충 이해하되, 4개의 props를 전달해줘야하구나 정도로만 알면 됨
-          drawerContent={props => <SideDrawer {...props} />}
-        >
-          <Drawer.Screen 
-            name="Home" 
-            component={DrawerHomeScreen}
-            options={{
-              //{/* 아이콘 넣는 방법2 */}
-              drawerIcon: () =>(
-                <Image
-                  //이미지 경로를 넣어줘야함
-                  source={PictogramHome}
-                  style={{width: 40, height: 40}}
-                />
-              )
-            }}
-          />
-          <Drawer.Screen name="User" component={DrawerUserScreen}/>
-        </Drawer.Navigator>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={TabHomeScreen} />
+          <Tab.Screen name="User" component={TabUserScreen} />
+        </Tab.Navigator>
       </NavigationContainer>
+      // <NavigationContainer>
+      //   <Drawer.Navigator
+      //     initialRouteName="Home"
+      //     drawerType="front"
+      //     // drawerType='slide' //화면 같이 밀림
+      //     // drawerType='permanent' //영원히 열림
+      //     drawerPosition='right' //오른쪽에서 열림
+      //     drawerStyle={{
+      //       backgroundColor:'#c6cbef',
+      //       width:200
+      //     }}
+      //     // 다른 페이지로 갈 수 있는 버튼 스타일주기
+      //     drawerContentOptions={{
+      //       activeTintColor:'red',
+      //       activeBackgroundColor:'skyblue'
+      //     }}
+      //     //drawerContetn를 커스터마이징함
+      //     //렌더링하기 위한 리엑트 요소를 반환함
+      //     //4개의 props를 받음
+      //     // 1. drawe navigator에 어떤 요소들이 있는지 알려주는 state
+      //     // 2. 화면 이동을 위한 navigation
+      //     // 3. drawer screen에 옵션을 담기위한 디스크립터스
+      //     // 4. drew가 열렸는지 판단하는 progress
+      //     //대충 이해하되, 4개의 props를 전달해줘야하구나 정도로만 알면 됨
+      //     drawerContent={props => <SideDrawer {...props} />}
+      //   >
+      //     <Drawer.Screen 
+      //       name="Home" 
+      //       component={DrawerHomeScreen}
+      //       options={{
+      //         //{/* 아이콘 넣는 방법2 */}
+      //         drawerIcon: () =>(
+      //           <Image
+      //             //이미지 경로를 넣어줘야함
+      //             source={PictogramHome}
+      //             style={{width: 40, height: 40}}
+      //           />
+      //         )
+      //       }}
+      //     />
+      //     <Drawer.Screen name="User" component={DrawerUserScreen}/>
+      //   </Drawer.Navigator>
+      // </NavigationContainer>
 
   // logoTitle = () =>{
   //   return(
