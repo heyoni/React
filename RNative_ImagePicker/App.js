efe1189cc5c8535affb6a28b5a2740ee083b1b90
heyoni <hhheyoni@gmail.com>
2020-11-23 23:43:08 +0900

    (1123) Image Picker
    Phone Resource 활용하기

---
+++ RNative_ImagePicker/App.js
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, Image, Button } from 'react-native'
+import ImagePicker from 'react-native-image-picker'
+
+class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>HELLO</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:'#e4ab26'
+  }
+})
+
+export default App;
