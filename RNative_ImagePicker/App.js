26a7dab2f156913d8d7f9851576cf38457a83fa6
heyoni <hhheyoni@gmail.com>
2020-11-24 00:01:12 +0900

    (1123) use camera & gallery

--- RNative_ImagePicker/App.js
+++ RNative_ImagePicker/App.js
@@ -11,10 +11,39 @@ import { StyleSheet, View, Text, Image, Button } from 'react-native'
 import ImagePicker from 'react-native-image-picker'
 
 class App extends Component {
+  state ={
+    avatar: ''
+  }
+  addImage = () => {
+    //카메라 구동하기
+    // ImagePicker.launchCamera({}, response=>{
+    //저장한 이미지 가져오기
+    // ImagePicker.launchImageLibrary({}, response=>{
+    
+    //카메라 구동하거나 저장한 이미지 가져오기
+    ImagePicker.showImagePicker({
+      title:'Choose your Photo',
+      takePhotoButtonTitle:'사진 찍기',
+      chooseFromLibraryButtonTitle: '앨범에서 불러오기',
+      cancelButtonTitle: '취소'
+    }, response=>{
+        this.setState({
+        avatar: response.uri
+      })
+    })
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>HELLO</Text>
+        <Image 
+          source={{uri:this.state.avatar}}
+          style={styles.avatar}
+        />
+
+        <Button
+          title="Add an Image"
+          onPress={()=> this.addImage()}
+        />
       </View>
     )
   }
@@ -26,6 +55,10 @@ const styles = StyleSheet.create({
     justifyContent:'center',
     alignItems:'center',
     backgroundColor:'#e4ab26'
+  },
+  avatar:{
+    width:'100%',
+    height:400
   }
 })
 
