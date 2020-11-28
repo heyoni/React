b82d7da47325c1a4aa010c183b32a54c4d6938e5
heyoni <hhheyoni@gmail.com>
2020-11-28 22:50:51 +0900

    (1128) Interpolation
    애니메이션 효과 중첩

--- RN_Animation/src/Animation01.js
+++ RN_Animation/src/Animation01.js
@@ -41,7 +41,12 @@ class AnimOne extends Component {
         {/* //애니메이션을 실행하도록 하는 부분 */}
             <Animated.View
                 style={{ //복잡한 방법
-                    opacity: this.state.mySquare//투명해지기
+                    opacity: this.state.mySquare,//투명해지기
+                    top: this.state.mySquare.interpolate({
+                        inputRange: [0, 1], //constructure 내부와 runAnimation내부에 선언된 값 => 투명도가 들어감(?) 작은 숫자부터 큰숫자로 넣어줘야함
+                        outputRange: [700, 0]//동시에 효과를 넣어주고 싶은 프로퍼티, 배열이 들어갈 수도 있음
+                    })//이동하는 애니메이션
+
                 }}
                 // style={this.state.mySquare.getLayout()} //간단한 방법
             >
