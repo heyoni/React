6fb0f86a7cfa7edbc9ea9aa2c83033a006131a71
heyoni <hhheyoni@gmail.com>
2020-11-28 23:07:47 +0900

    (1128) Interpolation2
    다양한 효과 한번에 주기

--- RN_Animation/src/Animation01.js
+++ RN_Animation/src/Animation01.js
@@ -39,20 +39,51 @@ class AnimOne extends Component {
     return (
        <View>
         {/* //애니메이션을 실행하도록 하는 부분 */}
-            <Animated.View
-                style={{ //복잡한 방법
-                    opacity: this.state.mySquare,//투명해지기
-                    top: this.state.mySquare.interpolate({
-                        inputRange: [0, 1], //constructure 내부와 runAnimation내부에 선언된 값 => 투명도가 들어감(?) 작은 숫자부터 큰숫자로 넣어줘야함
-                        outputRange: [700, 0]//동시에 효과를 넣어주고 싶은 프로퍼티, 배열이 들어갈 수도 있음
-                    })//이동하는 애니메이션
-
-                }}
+          <Animated.View
+            style={{ //복잡한 방법
+              opacity: this.state.mySquare,//투명해지기
+                transform: [
+                  {
+                    //top 대신 사용하는 방법
+                    // translateY: this.state.mySquare.interpolate({
+                    //     inputRange: [0, 1], 
+                    //     outputRange: [700, 0]
+                    rotateX: this.state.mySquare.interpolate({
+                      inputRange: [0, 0.5, 1], 
+                      outputRange: ['0deg', '180deg', '360deg']
+                    }),
+                  },
+                  {
+                    translateX: this.state.mySquare.interpolate({
+                      inputRange: [0, 0.5, 1], 
+                      outputRange: [300, 150, 0]
+                    })
+                  }
+                ]
+            // top: this.state.mySquare.interpolate({
+            //     inputRange: [0, 1], //constructure 내부와 runAnimation내부에 선언된 값 => 투명도가 들어감(?) 작은 숫자부터 큰숫자로 넣어줘야함
+            //     outputRange: [700, 0]//동시에 효과를 넣어주고 싶은 프로퍼티, 배열이 들어갈 수도 있음
+            // })//이동하는 애니메이션
+            }}
                 // style={this.state.mySquare.getLayout()} //간단한 방법
             >
                 <View style={styles.square}>
                 </View>
             </Animated.View>
+            <Animated.Text
+              style={{
+                fontSize:this.state.mySquare.interpolate({
+                    inputRange:[0, 0.5, 1],
+                    outputRange:[40, 30, 20]
+                }),
+                color:this.state.mySquare.interpolate({
+                    inputRange:[0, 0.5, 1],
+                    outputRange:['red', 'green', 'blue']
+                }),
+              }}
+            >
+                <Text>애니메이션 효과</Text>
+            </Animated.Text>
 
             <Button
                 title="애니메이션 시작"
