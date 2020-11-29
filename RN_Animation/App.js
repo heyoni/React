f1ba75ffd78ef41a7a98e356a291b83aaea33fff
heyoni <hhheyoni@gmail.com>
2020-11-30 00:22:50 +0900

    (1129) Platform
    플랫폼 별 다른 코딩 구현하기

--- RN_Animation/App.js
+++ RN_Animation/App.js
@@ -7,36 +7,71 @@
  */
 
 import React, { Component } from 'react';
-import { StyleSheet,View, Text } from 'react-native'
+import { Platform, StyleSheet,View, Text } from 'react-native'
 import AnimOne from './src/Animation01'
 import AnimTwo from './src/Animation02'
 import Supertext from './src/utils/supertext'
 
 //component = 재사용 가능한 템플릿을 만든다
 class App extends Component {
+  checkSupport = () => {
+    if (Platform.OS === 'ios'){
+      if (Platform.Version < 13.6){
+        return false
+      }
+    } else{
+      if (Platform.Version < 27){
+        return false
+      }
+    }
+    return true
+  }
+
+
   render() {
+    console.warn(Platform.Version)
     return (
       <View style={styles.container}>
-        {/* <Text> 컴포넌트 만들기 </Text> */}
-        {/* <AnimTwo/> */}
-
-        {/* 이 화면에서만 스타일을 다르게 주고 싶은 경우 */}
-        <Supertext
-          style={{backgroundColor:'red'}}
-        >
-          템플릿 입니다2.
-        </Supertext>
+        {
+          this.checkSupport() ?
+          <Supertext
+            // style={{backgroundColor:'red'}}
+            style={styles.div}
+          >
+            {/* 템플릿 입니다2. */}
+            {
+              Platform.OS === 'ios'?
+                '아이폰이네요'
+              :
+                '안드로이드이네요'
+            }
+          </Supertext>
+        :
+          <Text>
+            지원하지 않습니다.
+          </Text>
+        }
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  container :{
+  container: {
     flex:1,
     backgroundColor:'#bbb',
     alignItems:'center',
     justifyContent:'center',
+  },
+  div: {
+    ...Platform.select({
+      ios: {
+        backgroundColor:'skyblue'
+      },
+      android: {
+        backgroundColor:'yellow'
+      }
+    })
   }
 })
 
