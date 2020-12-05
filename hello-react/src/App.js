fbb32601f1886c17283b88141e693c471393aa59
heyoni <hhheyoni@gmail.com>
2020-12-06 00:09:18 +0900

    (1205) component2
    state

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -7,7 +7,7 @@ import MC from './MyComponent'
 class App extends Component{
   render(){
     return (
-      <MC name={3}/>
+      <MC name='fjifkk' age={3}/>
     );
   }
 }
--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -2,10 +2,25 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class MyComponent extends Component{
+
+  constructor(props) {
+      //컴포넌트의 생성자 메서드
+      super(props);
+      this.state = {
+          number: 1
+      }
+  }
   render(){
     return (
         <div>
-            안녕하세욤 저는 {this.props.name}입니다.
+            <p>안녕하세욤 저는 {this.props.name}입니다.</p>
+            <p>저는 {this.props.age}살 입니다.</p>
+            <p>이 메세지는 {this.state.number}번째로 표시되었습니다.</p>
+            <button onClick={() =>{
+                this.setState({
+                    number: this.state.number + 1
+                })
+            }}>추가하기</button>
         </div>
     )
   }
@@ -13,10 +28,10 @@ class MyComponent extends Component{
 // MyComponent.defaultProps = {
 //     name:'기본 이름'
 // }
-MyComponent.PropTypes = {
-    // props 검증하기
-    name : PropTypes.string
-}
+// MyComponent.PropTypes = {
+//     // props 검증하기
+//     name : PropTypes.string
+// }
 
 
 export default MyComponent;
