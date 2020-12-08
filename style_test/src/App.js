2c329f33f9a144fee7437913b9eb1593df3f5790
heyoni <hhheyoni@gmail.com>
2020-12-09 00:51:17 +0900

    (1208) sass button
    sass로 button 만들기, 이해 못함

--- style_test/src/App.js
+++ style_test/src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
-import styles from './App.module.css'
+// import styles from './App.module.css'
+import styles from './App.scss'
 import classNames from 'classnames/bind'
+import Button from './components/Button/Button';
 
 const cx = classNames.bind(styles)
 class App extends Component {
   render() {
+    const isBlue = true;
     return(
-      <div className={cx('box', 'blue')}></div>
+      // <div className={cx('box', {
+      //   blue: isBlue
+      // })}>
+      //   <div className={cx('box-inside')}></div>
+      // </div>
+      <div>
+        <Button>버튼</Button>
+      </div>
     )
   }
 }
