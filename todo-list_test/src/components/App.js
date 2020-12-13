1378d3d484fd64576d67f15ef6681a6138cfb1ae
heyoni <hhheyoni@gmail.com>
2020-12-14 00:10:01 +0900

    (1213) todo-list test

---
+++ todo-list_test/src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import PageTemplate from './PageTemplate';
+// import PageTemplate from 'src/components/PageTemplate.js'
+
+class App extends Component{
+    render() {
+        return (
+            <PageTemplate>일정 관리</PageTemplate>
+        )
+    }
+}
+
+export default App;
---
+++ todo-list_test/src/components/PageTemplate/index.js
@@ -0,0 +1,2 @@
+//경로 './PageTemplate/PageTemplate'에서 './PageTemplate'으로 줄이기위해서 사용
+export { default } from './PageTemplate'
