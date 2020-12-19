64dee6c8613fd8ad8da3591bfe49f54767e88e4f
heyoni <hhheyoni@gmail.com>
2020-12-20 02:57:05 +0900

    (1219) CORS -> proxy 사용하기

--- boiler-plate/client/src/App.js
+++ boiler-plate/client/src/App.js
@@ -14,16 +14,7 @@ function App() {
   return (
     <Router>
       <div>
-
         <hr />
-
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
         <Switch>
           <Route exact path="/" component={LandingPage} />
             {/* <LandingPage /> 위에 컴포넌트로 넣어주면 한줄로 가능 */}
--- boiler-plate/client/src/components/views/LandingPage/LandingPage.js
+++ boiler-plate/client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,16 @@
+import { response } from 'express'
 import React from 'react'
 
 function LandingPage() {
+
+    //이 페이지에 들어오자마자 실행되는 코드
+    userEffect(() => {
+        //get 요청을 서버에 보내줌 Endpoint는 '/api/hello'
+        axios.get('/api/hello')
+        //보낸 다음 서버에서 돌아오는 응답을 콘솔창에 띄움
+        .then(response=> {console.log(response.data)})
+    }, []) // 실행 안됨 -> CORS정책 때문에
+
     return (
         <div>
             LandingPage
