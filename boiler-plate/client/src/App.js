ad414e3179b5bc452c989dd1b3bce1d91c02833d
heyoni <hhheyoni@gmail.com>
2020-12-20 02:38:06 +0900

    (1218) React Router DOM

--- boiler-plate/client/src/App.js
+++ boiler-plate/client/src/App.js
@@ -1,12 +1,37 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
 
-
+import LandingPage from './components/views/LandingPage/LandingPage'
+import LoginPage from './components/views/LoginPage/LoginPage'
+import RegisterPage from './components/views/RegisterPage/RegisterPage'
 
 function App() {
   return (
-    <div>
-      APPAPAPAPAPP
-    </div>
+    <Router>
+      <div>
+
+        <hr />
+
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+            {/* <LandingPage /> 위에 컴포넌트로 넣어주면 한줄로 가능 */}
+          <Route path="/login" component={LoginPage} />
+          <Route path="/register" component={RegisterPage} />
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
