4f1f438839491caf15508b799521aaaa10e65faa
heyoni <hhheyoni@gmail.com>
2020-12-26 17:13:30 +0900

    (1226)Load more page btn

--- boilerplate-mern-stack-master/client/src/components/views/LandingPage/LandingPage.js
+++ boilerplate-mern-stack-master/client/src/components/views/LandingPage/LandingPage.js
@@ -13,22 +13,30 @@ const textZip = (props) => { //영화설명이 너무 길어서 임의로 줄이
 function LandingPage() {
     const [Movies, setMovies] = useState([])//가져온 정보(이미지, 영화이름 등)를 넣어줌
     const [MainMovieImage, setMainMovieImage] = useState(null)
+    const [CurrentPage, setCurrentPage] = useState(0)
 
 
-
-
-    useEffect(()=>{
-        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`
-
+    const fetchMovies = (endpoint) =>{
+        // useEffect, loadMoreItems에 중복된 내용을 넣어줌
         fetch(endpoint)
             .then(response => response.json())//json 형태로 변환하고
             .then(response => {
                 console.log(response)
-                setMovies([...response.results])
+                setMovies([...Movies, ...response.results]) //원래 있던 무비에 새로 가져온 것들을 추가해줌(loadMoreItems)
                 setMainMovieImage(response.results[0])//가장 인기있는 영화를 가져옴, 이걸 MainMovie컴포넌트에 넘겨줄 것
+                setCurrentPage(response.page)
             })//그 값을 가져옴
+    } 
 
+    useEffect(()=>{
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+        fetchMovies(endpoint)
     }, [])
+
+    const loadMoreItems = () =>{
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${CurrentPage + 1}`
+        fetchMovies(endpoint)
+    }
     return (
         <div style={{ width: '100%', margin: '0'}}>
                 {/* const a = {MainMovieImage.overview}
@@ -56,7 +64,7 @@ function LandingPage() {
                     <React.Fragment key={index}>
                         <GridCards 
                             image={movie.poster_path ? 
-                                `${IMAGE_BASE_URL}w500${movie.poster_path}` : null}
+                                `${IMAGE_BASE_URL}w200${movie.poster_path}` : null}
                             movieId={movie.id} //고유의 영화 정보로 들어가기 위해서 id값을 가져옴
                             movieName={movie.original_title}
                         
@@ -68,7 +76,7 @@ function LandingPage() {
             </div>
 
             <div style={{ display:'flex', justifyContent:'center'}}>
-                <button>Load more</button>
+                <button onClick={loadMoreItems}>Load more</button>
             </div>
         </div>
     )
--- boilerplate-mern-stack-master/client/src/components/views/commons/GridCards.js
+++ boilerplate-mern-stack-master/client/src/components/views/commons/GridCards.js
@@ -7,7 +7,7 @@ function GridCards(props) {
         <Col lg={6} md={8} xs={24}>
             <div style={{position: 'relative'}}>
                 <a href={`/movie/${props.movieId}`}>
-                    <img style={{width:'100%',height:'555px'}} src={props.image} alt={props.movieName}/>
+                    <img style={{width:'100%',height:'50%'}} src={props.image} alt={props.movieName}/>
                 </a>
             </div>
         </Col>
