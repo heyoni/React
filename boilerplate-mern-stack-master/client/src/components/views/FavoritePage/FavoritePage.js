1985bbe8165439c6610d4576e6f25e27578c4a2b
heyoni <hhheyoni@gmail.com>
2020-12-28 19:15:56 +0900

    (1228) ERROR NOT FIXED
    :-1:

--- boilerplate-mern-stack-master/client/src/components/views/FavoritePage/FavoritePage.js
+++ boilerplate-mern-stack-master/client/src/components/views/FavoritePage/FavoritePage.js
@@ -11,52 +11,90 @@ function FavoritePage() {
     const [Favorites, setFavorites] = useState([])
 
     useEffect(() => {
-        Axios.post('/api/favorite/getFavoriteMovie', { userFrom: localStorage.getItem('userId') })
-            .then(response => {
-                if(response.data.success){
-                    console.log(response.data)
-                    setFavorites(response.data.favorites)
-                } else {
-                    alert('영화정보를 가져오는데 실패했습니다.')
-                }
-            })
+
+        fetchFavoredMovie()
+       
     }, [])
 
 
-    const renderCards = Favorites.map((favorite, index)=> {
+    const fetchFavoredMovie = () => {
+        Axios.post('/api/favorite/getFavoritedMovie', { userFrom: localStorage.getItem('userId') })
+            .then(response => {
+                if (response.data.success) {
+                    setFavorites(response.data.favorites)
+                } else {
+                    alert('영화 정보를 가져오는데 실패 했습니다.')
+                }
+            })
+    }
+
+
+
+    const onClickDelete = (movieId, userFrom) => {
+
+        const variables = {
+            movieId,
+            userFrom
+        }
+
+        Axios.post('/api/favorite/removeFromFavorite', variables)
+            .then(response => {
+                if (response.data.success) {
+                    fetchFavoredMovie()
+                } else {
+                    alert("리스트에서 지우는데 실패했습니다.")
+                }
+            })
+
+
+    }
+
+
+    const renderCards = Favorites.map((favorite, index) => {
 
         const content = (
             <div>
                 {favorite.moviePost ?
-                    <img src={`${IMAGE_BASE_URL}w500${favorite.moviePost}`} /> : "NO IMAGE"
+
+                    <img src={`${IMAGE_BASE_URL}w500${favorite.moviePost}`} /> : "no image"
+
                 }
             </div>
         )
+
+
         return <tr key={index}>
-            <Popover content={content} title={`${favorite.movieTitle}`}>
+
+            <Popover content={content} title={`${favorite.movieTitle}`} >
                 <td>{favorite.movieTitle}</td>
             </Popover>
-            <td>{favorite.movieRuntTime} mins</td>
-            <td><button>remove</button></td>
+
+            <td>{favorite.movieRunTime} mins</td>
+            <td><button onClick={() => onClickDelete(favorite.movieId, favorite.userFrom)}>Remove</button></td>
 
         </tr>
     })
 
+
+
     return (
-        <div style={{width:'85%', margin:'3rem auto'}}>
-            <h2>Favorite Movies</h2>
+        <div style={{ width: '85%', margin: '3rem auto' }}>
+            <h2> Favorite Movies </h2>
             <hr />
 
             <table>
                 <thead>
                     <tr>
                         <th>Movie Title</th>
-                        <th>Movie Runtime</th>
-                        <td>Remove From favorites</td>
+                        <th>Movie RunTime</th>
+                        <td>Remove from favorites</td>
                     </tr>
                 </thead>
                 <tbody>
+
+
                     {renderCards}
+
 
                 </tbody>
             </table>
