794a6c1ddbcec6e5c0f102f23d5085a93904532e
heyoni <hhheyoni@gmail.com>
2020-12-31 00:36:11 +0900

    (1230) Video Upload

--- boilerplate/client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ boilerplate/client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -3,16 +3,43 @@ import { Typography, Button, Form, message, Input, Icon } from 'antd'
 import DropZone from 'react-dropzone'
 const { TextArea } = Input;
 const { Title } = Typography;
+
 const PrivateOptions = [
     {value:0, label:'Private'},
     {value:1, label:'Public'}
 ]
+
+const CategoryOptions = [
+    {value:0, label:'Film & Animation'},
+    {value:1, label:'Autos & Vehicles'},
+    {value:2, label:'Music'},
+    {value:3, label:'Pets'},
+    
+
+]
+
 
 function VideoUploadPage() {
     const [VideoTitle, setVideoTitle] = useState('')
     const [Description, setDescription] = useState('')
     const [Private, setPrivate] = useState(0)
     const [Category, setCategory] = useState('film & Animation')
+
+    const onTitleChange = (e) => {
+        setVideoTitle(e.currentTarget.value)
+    }
+
+    const onDescritionChange = (e) => {
+        setDescription(e.currentTarget.value)
+    }
+
+    const onPrivateChange = (e) => {
+        setPrivate(e.currentTarget.value)
+    }
+
+    const onCategoryChange = (e) => {
+        setPrivate(e.currentTarget.value)
+    }
 
     return (
         <div style={{ maxWidth: '700px', margin:'2rem auto'}}>
@@ -46,29 +73,31 @@ function VideoUploadPage() {
                 <br />
                 <label>Title</label>
                 <Input 
-                    onChange
+                    onChange={onTitleChange}
                     value={VideoTitle}
                 />
                 <br />
                 <br />
                 <label>Description</label>
                 <TextArea
-                    onChange
+                    onChange={onDescritionChange}
                     value={Description}
                 />
                 <br />
                 <br />
 
-                <select onChange>
+                <select onChange={onPrivateChange}>
                     {PrivateOptions.map((item, index) => (
-                        <option key={index} value={item.value}></option>
+                        <option key={index} value={item.value}>{item.label}</option>
                     ))}
                 </select>
 
                 <br />
                 <br />
-                <select onChange>
-                    <option key value></option>
+                <select onChange={onCategoryChange}>
+                    {CategoryOptions.map((item, index) => (
+                        <option key={index} value={item.value}>{item.label}</option>
+                    ))}
                 </select>
                 <br />
                 <br />
